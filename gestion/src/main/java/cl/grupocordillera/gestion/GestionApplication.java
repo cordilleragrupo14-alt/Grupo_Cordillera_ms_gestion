217d04bcf494b5f5fa95f60b3a3a5751799d0ecc
Sicr0911 <seb.catalanr@duocuc.cl>
2026-07-10 02:15:26 -0400

    Fix: cambiar basePackage por basePackages en GestionApplication

--- gestion/src/main/java/cl/grupocordillera/gestion/GestionApplication.java
+++ gestion/src/main/java/cl/grupocordillera/gestion/GestionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackage = "cl.grupocordillera.gestion.client")
+@EnableFeignClients(basePackages = "cl.grupocordillera.gestion.client")
 public class GestionApplication {
     public static void main(String[] args) {
         SpringApplication.run(GestionApplication.class, args);
